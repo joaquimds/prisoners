3bcd219ee89911a66663d8b8a64ecb1138ae01a8
Joaquim d'Souza <joaquimds@gmail.com>
2018-12-12 20:03:19 +0000

    add alert when game starts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,9 +7,27 @@ import Main from '../main/main'
 import Chat from '../chat/chat'
 import { subscribe } from '../../actions/socket'
 
+const idleTitle = document.title
+const playingTitle = document.title + ' | Playing'
+
 class App extends Component {
   componentDidMount () {
+    this.titleAlertHandle = null
     this.props.subscribe()
+    document.addEventListener('visibilitychange', () => {
+      if (!document.hidden) {
+        this.hideTitleAlert()
+      }
+    })
+  }
+  componentDidUpdate (prevProps) {
+    if (!this.props.dilemma) {
+      document.title = idleTitle
+      return
+    }
+    if (!prevProps.dilemma) {
+      this.showTitleAlert()
+    }
   }
   componentWillUnmount () {
     this.props.unsubscribe && this.props.unsubscribe()
@@ -22,9 +40,25 @@ class App extends Component {
       </div>
     )
   }
+  showTitleAlert () {
+    document.title = playingTitle
+    if (!document.hidden) {
+      return
+    }
+    this.titleAlertHandle = setInterval(() => {
+      document.title = document.title === playingTitle ? idleTitle : playingTitle
+    }, 2000)
+  }
+  hideTitleAlert () {
+    if (this.titleAlertHandle !== null) {
+      clearInterval(this.titleAlertHandle)
+    }
+    this.titleAlertHandle = null
+    document.title = this.props.dilemma ? playingTitle : idleTitle
+  }
 }
 
-const mapStateToProps = ({ socket }) => ({ unsubscribe: socket.unsubscribe })
+const mapStateToProps = ({ socket, dilemma }) => ({ unsubscribe: socket.unsubscribe, dilemma })
 const mapDispatchToProps = (dispatch) => {
   return {
     subscribe: () => {
